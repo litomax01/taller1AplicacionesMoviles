52ab44269f3829b83ce8191b327ba1f1c135dc6e
litomax01 <litomax01@hotmail.com>
2025-02-01 10:20:49 -0500

    hi

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/homescreen';
+import homeScreen from './screens/homescreen';
 import ProductScreen from './screens/productscreen';
 import CartScreen from './screens/carritoscreen';
+import LoginScreen from './screens/loginscreen';
 import { CartProvider } from './context/carritocontexto';
 
 const Stack = createStackNavigator();
@@ -12,10 +13,11 @@ export default function App() {
   return (
     <CartProvider>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Product" component={ProductScreen} />
-          <Stack.Screen name="Cart" component={CartScreen} />
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} options={{ title: 'Inicio de Sesión' }} />
+          <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Tienda' }} />
+          <Stack.Screen name="Product" component={ProductScreen} options={{ title: 'Detalle del Producto' }} />
+          <Stack.Screen name="Cart" component={CartScreen} options={{ title: 'Carrito de Compras' }} />
         </Stack.Navigator>
       </NavigationContainer>
     </CartProvider>
